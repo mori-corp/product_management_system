c9aee1e4ea8d24a71d6b460b063963395cb7b81d
Shintaro <mori@hey.com>
2022-11-22 13:09:26 +0900

    initialized app config

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDCBv2v0c3AhlVljVt0zcbtdzcBOj0WB5o",
+  authDomain: "product-management-system-2022.firebaseapp.com",
+  projectId: "product-management-system-2022",
+  storageBucket: "product-management-system-2022.appspot.com",
+  messagingSenderId: "299955588255",
+  appId: "1:299955588255:web:72e54dd37931cf5ba966aa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
